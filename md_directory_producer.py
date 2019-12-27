54c50ec32687ebbf5ba2cd87bd82b78d28704802
loopyme <peter@mail.loopy.tech>
2019-12-27 20:50:12 +0800

    modified:   md_directory_producer.py

--- md_directory_producer.py
+++ md_directory_producer.py
@@ -13,7 +13,6 @@ import os
 
 
 class DirectoryProducer:
-
     manual_check = True
     default_max_title_level = 3
     default_root_path = "./"
@@ -21,82 +20,127 @@ class DirectoryProducer:
 
     @classmethod
     def config(
-        cls,
-        default_max_title_level=None,
-        manual_check=None,
-        default_result_path=None,
-        default_root_path=None,
+            cls,
+            manual_check: bool = None,
+            default_max_title_level: int = None,
+            default_result_path: str = None,
+            default_root_path: str = None,
     ):
+        """
+        Config the DirectoryProducer
+
+        :param manual_check: If this value set to true, each directory file needs manual validation before\
+         written to the file. Otherwise, all manual validation will be skipped
+        :param default_max_title_level: Titles will not be recorded with deeper level
+        :param default_result_path: The path of the result directory files(Both relative and absolute addresses are\
+         acceptable)
+        :param default_root_path: The path of the markdown files
+        :return:
+        """
+        cls.manual_check = (
+            manual_check if manual_check is not None
+            else cls.manual_check
+        )
         cls.default_max_title_level = (
-            default_max_title_level
-            if default_max_title_level is not None
+            default_max_title_level if default_max_title_level is not None
             else cls.default_max_title_level
         )
-        cls.manual_check = (
-            manual_check if manual_check is not None else cls.manual_check
-        )
         cls.default_root_path = (
-            default_root_path
-            if default_root_path is not None
+            default_root_path if default_root_path is not None
             else cls.default_root_path
         )
         cls.default_result_path = (
-            default_result_path
-            if default_result_path is not None
+            default_result_path if default_result_path is not None
             else cls.default_result_path
         )
 
     def __init__(
-        self,
-        result_filename,
-        is_target_file,
-        max_title_level=None,
-        result_title=None,
-        root_path=None,
-        result_path=None,
-        is_sorted = False,
+            self,
+            result_filename: str,
+            is_target_file,
+            is_sorted: bool = False,
+            max_title_level: int = None,
+            result_title: str = None,
+            root_path: str = None,
+            result_path: str = None,
     ):
-        self.max_title_level = (
-            max_title_level
-            if max_title_level is not None
-            else self.default_max_title_level
-        )
-        self.root_path = root_path if root_path is not None else self.default_root_path
-        self.result_path = (
-            result_path if result_path is not None else self.default_result_path
-        )
-        self.result_title = (
-            result_title if result_title is not None else result_filename
-        )
+        """
+        init func
+
+        :type is_target_file: function or tuple
+
+        :param result_filename: The name of result directory file
+        :param is_target_file: If a function is passed in, it will be used as a file valid function. If a tuple is\
+         passed in, the default validate function will be used.
+        :param is_sorted: It indicates whether need to sort the titles.
+        :param max_title_level: See classmethod:config:default_max_title_level. If none is passed in,\
+         the default parameters of the class are used
+        :param result_title: If none is passed in, result_filename will be used as title.
+        :param root_path: See classmethod:config:default_root_path. If none is passed in,\
+         the default parameters of the class are used
+        :param result_path: See classmethod:config:default_result_path. If none is passed in,\
+         the default parameters of the class are used
+        """
         self.result_filename = result_filename
         self.is_sorted = is_sorted
+
+        self.max_title_level = (
+            max_title_level if max_title_level is not None
+            else self.default_max_title_level
+        )
+        self.root_path = (
+            root_path if root_path is not None
+            else self.default_root_path
+        )
+        self.result_path = (
+            result_path if result_path is not None
+            else self.default_result_path
+        )
+        self.result_title = (
+            result_title if result_title is not None
+            else result_filename
+        )
+
         if hasattr(is_target_file, "__call__"):
             self.is_target_file = is_target_file
+        elif len(is_target_file) == 2 and isinstance(is_target_file[0], int) and isinstance(is_target_file[1], int):
+            self.is_target_file = lambda filename: \
+                self.__is_target_file_auto(filename, *is_target_file)
         else:
-            self.is_target_file = lambda filename: self.is_target_file_auto(
-                filename, *is_target_file
-            )
+            raise AttributeError("Param 'is_target_file' is invalid.It should be a function or tuple.")
+
+        self._check_param()
+
+    def _check_param(self):
+        if not hasattr(self.is_target_file, "__call__"):
+            raise AttributeError("Param 'is_target_file' is invalid.It should be a function or tuple.")
+        elif not os.path.isdir(self.root_path):
+            raise AttributeError("Param 'root_path' is invalid. It should be a dir.")
+        elif not os.path.isdir(self.result_path):
+            raise AttributeError("Param 'result_path' is invalid. It should be a dir.")
+        else:
+            return
 
     @staticmethod
-    def is_target_file_auto(file_name, limit_low, limit_high):
+    def __is_target_file_auto(file_name, limit_low, limit_high):
         if file_name.__contains__("."):  # is not a directory
             file_name, file_type = file_name.split(".")[0:2]
             if (
-                file_type == "md"
-                and file_name.isdigit()
-                and limit_low <= int(file_name) < limit_high
+                    file_type == "md"
+                    and file_name.isdigit()
+                    and limit_low <= int(file_name) < limit_high
             ):
                 return True
         return False
 
     @staticmethod
-    def title_format(title):
+    def _title_format(title):
         """transfer the *title* to readable format"""
         title = re.sub("#+? |\n", "", title)
         title = re.sub("\\\\. ", " ", title)
         return title
 
-    def title_to_link(self, file_name, title):
+    def _title_to_link(self, file_name, title):
         """transfer the *title* in the *file_name* to relative link"""
         title_dic = {
             r"\n|\.|\\\\|": r"",
@@ -115,7 +159,7 @@ class DirectoryProducer:
         id = path + title
         return id.lower()
 
-    def titles_sort(self, titles):
+    def _titles_sort(self, titles):
         """sort the *titles* list"""
 
         # title to index
@@ -149,8 +193,8 @@ class DirectoryProducer:
                     for t in titles:
                         t_level = t.count("#")
                         if t_level < self.max_title_level:
-                            t_title = self.title_format(t)
-                            t_link = self.title_to_link(file, t)
+                            t_title = self._title_format(t)
+                            t_link = self._title_to_link(file, t)
 
                             levels[t_title] = t_level
                             links[t_title] = t_link
@@ -159,7 +203,7 @@ class DirectoryProducer:
         if self.is_sorted:
             titles_sorted = links.keys()
         else:
-            titles_sorted = self.titles_sort(links.keys())
+            titles_sorted = self._titles_sort(links.keys())
 
         # produce the directory
         directory = self.result_title + "\n"
@@ -168,7 +212,7 @@ class DirectoryProducer:
             directory += "* [{}]({})\n".format(t, links[t])
 
         # check and write to file
-        path = self.result_path + self.title_format(self.result_filename)
+        path = self.result_path + self._title_format(self.result_filename)
         if self.manual_check:
             print(
                 directory[:500]
